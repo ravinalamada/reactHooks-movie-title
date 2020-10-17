d2eb20d27ad3dda25c6f4fad2752ba6cb10a3299
Prisca Roseline <prisca.ros@onja.org>
2020-10-17 15:54:43 +0300

    I updated movies List.

---
+++ component/MovieComponent.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+
+function MovieComponent() {
+  let [movies, setMovies] = useState([]);
+
+  async function FetchMovie() {
+    const response = await fetch(`https://ghibliapi.herokuapp.com/films`);
+    let data = await response.json();
+    setMovies(() => movies = data);
+    console.log(movies);
+  }
+
+  useEffect(() => {
+    FetchMovie()
+  }, []);
+
+  return (
+    <section>
+      {movies && movies.map(item => {
+        return (
+          <div key={item.id} className="container">
+            <div className="wrapper">
+              <h2>{item.title}</h2>
+              <p>{item.release_date}</p>
+              <p>{item.rt_score}</p>
+            </div>
+            <p>{item.description}</p>
+            <div className="wrapper">
+              <p>{item.producer}</p>
+              <p>{item.director}</p>
+            </div>
+
+          </div>
+        )
+      })}
+    </section>
+  )
+
+}
+
+
+
+
+export default MovieComponent;
+
+
---
+++ pages/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MovieComponent from "../component/MovieComponent";
+
+function App() {
+  return (
+    <main>
+      <h1>Movies List</h1>
+      <MovieComponent></MovieComponent>
+    </main>
+  )
+}
+
+export default App;
